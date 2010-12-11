82f33a463d2d90c43c6ab0079ff88fb04a1b35d0
Kim Burgestrand <kim@burgestrand.se>
2010-12-11 02:03:34 +0100

    Add pending specs

--- spec/keeper_spec.rb
+++ spec/keeper_spec.rb
@@ -1,7 +1,26 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Keeper" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  describe "#wait_for" do
+    context "new event" do
+      it "should create the event signaller"
+      it "should register the calling thread as waiting"
+    end
+    
+    context "existing event" do
+      it "should re-use the existing signaller"
+      it "should not affect any other waiting threads"
+      it "should register the the calling thread as waiting"
+    end
+  end
+  
+  describe "#fire" do
+    context "existing event" do
+      it "should release all listeners"
+    end
+    
+    context "non-existing event" do
+      it "should not cause an error"
+    end
   end
 end
